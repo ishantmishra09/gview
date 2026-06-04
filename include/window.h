c052ae90fa402dc6b014a8f4aa10d6a13267fa0d
ishantmishra09 <ishantmishra01001@gmail.com>
2026-06-04 20:14:01 +0545

    refactor: replace GTK with SDL2 for rendering

--- include/window.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <gtk/gtk.h>
-
-#define GVIEW_WINDOW_DEFAULT_WIDTH 900
-#define GVIEW_WINDOW_DEFAULT_HEIGHT 600
-#define GVIEW_WINDOW_TITLE "gview"
-
-GtkWidget *gview_window_new(GtkApplication *app);
