56739ae3047f44f2fd4ca11a7729839784a0486b
ishantmishra09 <285984711+ishantmishra09@users.noreply.github.com>
2026-06-05 17:25:48 +0545

    refactor: replace GTK with SDL2 for rendering (#1)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,73 @@
-#include "app.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define WINDOW_TITLE "gview"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 int main(int argc, char *argv[]) {
 
-  GtkApplication *app = gview_app_new();
-  int status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-  return status;
+  (void)argc;
+  (void)argv;
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+
+    fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow(
+      WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+  if (!window) {
+
+    fprintf(stderr, "SDL_CreateWindow error: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (!renderer) {
+
+    fprintf(stderr, "SDL_CreateRenderer error: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running = true;
+  SDL_Event event;
+
+  while (running) {
+
+    while (SDL_PollEvent(&event)) {
+
+      switch (event.type) {
+
+      case SDL_QUIT:
+        running = false;
+        break;
+
+      case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+          running = false;
+        break;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30,
+                           255); /* dark grey background */
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
