1f62f4ae312920d506c6e295d03b318925897875
xinfnul <285984711+xinfnul@users.noreply.github.com>
2026-06-06 15:55:27 +0545

    feat(viewer): add NFD file open dialog

---
+++ src/nfd_g.c
@@ -0,0 +1,22 @@
+#include "nfd_g.h"
+
+#include <nfd.h>
+#include <stdio.h>
+
+nfdchar_t *open_dialog(void) {
+
+  nfdchar_t *outPath = NULL;
+
+  nfdfilteritem_t filters[] = {{"Images", "png,jpg,jpeg,bmp,ppm"}};
+
+  nfdresult_t result = NFD_OpenDialogU8(&outPath, filters, 1, NULL);
+  if (result == NFD_OKAY) {
+
+    return outPath;
+  } else if (result == NFD_ERROR) {
+
+    fprintf(stderr, "NFD error: %s\n", NFD_GetError());
+  }
+
+  return NULL;
+}
